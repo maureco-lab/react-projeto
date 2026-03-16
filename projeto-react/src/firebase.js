84622e841b6ecc7304ad5649243bc177776e06f7
maureco-lab <mauriciomrm04@gmail.com>
2026-03-16 19:49:03 -0300

    firebase

---
+++ projeto-react/src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  // Cole seus dados aqui
+  apiKey: "AIzaSyCcrn8TaTaWTVshZXCZG56ltuklPs9xLpU",
+  authDomain: "projeto-react-b41a0.firebaseapp.com",
+  databaseURL: "https://SEU_PROJETO.firebaseio.com",
+  projectId: "projeto-react-b41a0",
+  storageBucket: "projeto-react-b41a0.firebasestorage.app",
+  messagingSenderId: "968263915631",
+  appId: "1:968263915631:web:ed925305ad4559c270959a"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
